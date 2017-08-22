53e7a49e09380aac66cd59486a86d7d1e47119af
takuma_bishan.abewadana <takuma_bishan.abewadana@supership.jp>
2017-08-22 17:44:07 +0900

    fix js to es6

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,16 @@
-$(function() {
+$(() => {
   // make options of departure IC
-  $('#select_from_prefectures').change(function() {
+  $('#select_from_prefectures').change(() => {
     makeOptions('#select_from_prefectures', '#select_from_ic');
   });
 
   // make options of arrival IC
-  $('#select_to_prefectures').change(function() {
+  $('#select_to_prefectures').change(() => {
     makeOptions('#select_to_prefectures', '#select_to_ic');
   });
 
   // make ic options by selected prefectures
-  const makeOptions = function(psel, csel) {
+  const makeOptions = (psel, csel) => {
     $.ajax({
       type: "POST",
       url: "/options",
@@ -20,8 +20,8 @@ $(function() {
       headers: {
         "X-CSRF-Token": $("meta[name=csrf-token]").attr("content"),
       },
-    }).done(function(data) {
-      var options = $.map(data, function(val) {
+    }).done((data) => {
+      var options = $.map(data, (val) => {
         return `<option value="${val}">${val}</option>`;
       });
       $(csel).empty();
