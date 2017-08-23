36f0514bda5cdaae300710ed4f9293aeddb1f755
takuma_bishan.abewadana <takuma_bishan.abewadana@supership.jp>
2017-08-23 18:07:22 +0900

    add deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# change branch to release
+git checkout release
+
+# merge master to release branch
+git merge master
+
+# exec cmd before deploy to heroku
+npm run build
+
+# push to release branch
+git add .
+git commit -m "Deploying to Heroku"
+git push origin release
+
+# deploy to heroku
+git push heroku release:master
+
+# change branch to master
+git checkout master
