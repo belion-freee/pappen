991c146043d3fc03393072b2a1aea6c6da337628
takuma_bishan.abewadana <takuma_bishan.abewadana@supership.jp>
2017-08-23 18:23:53 +0900

    add import statement cable.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// app/assets/javascripts/cable.js
 //= require rails-ujs
 //= require jquery
 //= require turbolinks
