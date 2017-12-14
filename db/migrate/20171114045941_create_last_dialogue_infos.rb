12a0ed8c89896f892fbe4ffa7f57cc75934bd165
freee.belion <belion.freee@gmail.com>
2017-12-14 14:34:12 +0900

    add

--- db/migrate/20171114045941_create_last_dialogue_infos.rb
+++ db/migrate/20171114045941_create_last_dialogue_infos.rb
@@ -1,11 +1,13 @@
 class CreateLastDialogueInfos < ActiveRecord::Migration[5.1]
   def change
     create_table :last_dialogue_infos do |t|
-      t.string :uid
-      t.string :mode
-      t.string :context
+      t.string :uid, null: false
+      t.string :mode, null: false
+      t.string :context, null: false
 
       t.timestamps
     end
+
+    add_index :last_dialogue_infos, :uid, unique: true
   end
 end
