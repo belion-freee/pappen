1de6638c0260a5ca6e3c0c32a63cab2360aa5b83
freee.belion <belion.freee@gmail.com>
2017-12-14 13:29:52 +0900

    add db #5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213065952) do
+ActiveRecord::Schema.define(version: 20171214042551) do
 
   create_table "last_dialogue_infos", force: :cascade do |t|
     t.string "uid", null: false
