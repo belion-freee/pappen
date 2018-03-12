b5195be10ab866041f4bcf69dc1a5dcc79e628fc
freee.belion <belion.freee@gmail.com>
2018-03-12 20:05:31 +0900

    fuck #6

--- app/worker/sns/line/base.rb
+++ app/worker/sns/line/base.rb
@@ -55,8 +55,9 @@ class Sns::Line::Base
     end
 
     def raise_error?(param, res)
-      Rails.logger.error("raise_error? error params : #{params}")
+      Rails.logger.error("raise_error? error params : #{param}")
       Rails.logger.error("raise_error? error res : #{res}")
+      Rails.logger.error("raise_error? error res : #{res.body}")
       raise_error(param: param, code: res.try(:code), body: res.try(:body)) unless res.try(:code) == SUCCESS
     end
 
