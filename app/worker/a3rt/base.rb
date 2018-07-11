13e6da2990db475446b4748f3aac417eca791949
freee.belion <belion.freee@gmail.com>
2018-07-11 19:28:58 +0900

    バリエーションを増やす

--- app/worker/a3rt/base.rb
+++ app/worker/a3rt/base.rb
@@ -1,6 +1,12 @@
 class A3rt::Base
   include ApplicationWorker
 
+  MESSAGES = {
+    %w[ちんちん ちんこ チンコ まんこ マンコ えっち エッチ セックス] => %w[セクハラ、死ね 変態、滅しろ チ◯コ切るぞ？ てめえは殺処分],
+    %w[うんこ ウンコ うんち ウンチ クソ くそ] => %w[てめえの存在がうんこだわ お前の口からクソの匂いがする 顔がうんこみたい 存在がうんこ以下 お前喋れるんだ、うんこかと思ったわ],
+    %w[つまんね つまんな 退屈] => %w[てめえよりマシ 奇遇だな 貴様の存在がな],
+  }.freeze
+
   def initialize
     init(Settings.account.a3rt.uri.talk, apikey: env(:a3rt_api_key))
   end
@@ -10,7 +16,15 @@ class A3rt::Base
 
     @params[:query] = word
 
-    format_response(exec_request(:post))
+    ress = MESSAGES.select {|keys, _| keys.any? {|key| word.include?(key) } }
+
+    if ress.present?
+      res = ress.values.first
+      size = res.size - 1
+      res[rand(0..size)]
+    else
+      format_response(exec_request(:post))
+    end
   end
 
   protected
@@ -22,7 +36,7 @@ class A3rt::Base
       res["results"].first["reply"]
     else
       Rails.logger.error("message : #{res["message"]}")
-      "その質問には答えられない。。。すまぬ"
+      "さえずるな、愚民が"
     end
   end
 end
