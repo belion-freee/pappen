a51a408a58cc96ebd30b4745ff039e7c8c96bca2
belion-freee <belion.freee@gmail.com>
2018-10-05 15:56:59 +0900

    [Modified] final format!!

--- app/assets/javascripts/part/form.js
+++ app/assets/javascripts/part/form.js
@@ -27,4 +27,54 @@ $(function(){
      target.attr("disabled", "disabled");
    }
   });
+
+  $("#new-maxims").click(function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+        const category = $(this).data("category")
+        const url = $(this).data("url")
+
+        // store form HTML markup in a JS variable
+        var formTemplate = $('#form-template > form');
+
+        swal({
+          title: 'New Remarks',
+          html: formTemplate,
+          showCancelButton: true,
+          confirmButtonText: 'OK',
+          preConfirm: function preConfirm() {
+            var form = $("#swal2-content > form");
+            var body = {
+              category: form.find("#maxim_category option:selected").val(),
+              remark: form.find("#maxim_remark").val(),
+              author: form.find("#maxim_author").val(),
+              source: form.find("#maxim_source").val(),
+              url: form.find("#maxim_url").val()
+            };
+            return fetch(url + ".json", {
+              method: 'POST',
+              headers: {
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify(body)
+            }).then(function (response) {
+              if (response.ok) {
+                return response.json();
+              } else {
+                response.json().then(function (json) {
+                  swal.showValidationError(json.errors.join("<br>"));
+                });
+              }
+            });
+          },
+          allowOutsideClick: function allowOutsideClick() {
+            return !swal.isLoading();
+          }
+        }).then(function (result) {
+          if (result.value) {
+            swal({ type: 'success', title: 'Success!', footer: '<a href="' + url + '">Show All Remarks</a>' });
+          }
+        });
+    });
 });
