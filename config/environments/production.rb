4d31259e2bad88837cbed7ecdb80641d3fe334aa
belion-freee <belion.freee@gmail.com>
2019-01-10 16:55:16 +0900

    for remove warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ config.webpacker.check_yarn_integrity = false
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   # config.serve_static_assets = false
   # config.assets.compress = true
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
