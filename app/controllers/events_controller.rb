b1dd9ce09927f6658bde8bac55746c0a30fa0dba
belion-freee <belion.freee@gmail.com>
2020-04-04 09:21:01 +0900

    remove events column #66

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,7 +53,7 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      format_params params.require(:event).permit(:name, :place, :start, :end, :memo, room_member_ids: [])
+      format_params params.require(:event).permit(:name, :memo, room_member_ids: [])
     end
 
     def format_params(permitted_params)
